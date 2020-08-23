f98ab4f4b6f1ccff1e16b08b56ae6fb0c56dfa1b
BackGrowZ <anthony.carreta@gmail.com>
2020-08-23 14:02:41 +0200

    Add link to call and email on contact

--- src/componentes/Contact.js
+++ src/componentes/Contact.js
@@ -14,8 +14,8 @@ export default function Contact() {
                 </h1>
             </div>
             <div id='all-contact'>
-                <CardFlip title='Téléphone' description='06 58 93 04 21' taille='200px' classOf='contact-info' margin='10px' link={null} techno={null} visuel={Tel} />
-                <CardFlip title='Email' description='anthony.carreta@gmail.com' taille='200px' classOf='contact-info' margin='10px' link={null} techno={null} visuel={Email} />
+                <CardFlip title='Téléphone' description={<a style={{color:'inherit'}} target='_blank' rel='noopener noreferrer' href='tel:+33658930421'>06 58 93 04 21</a>} taille='200px' classOf='contact-info' margin='10px' link={null} techno={null} visuel={Tel} />
+                <CardFlip title='Email' description={<a style={{color:'inherit'}} target='_blank' rel='noopener noreferrer' href='mailto:anthony.carreta@gmail.com'>anthony.carreta@gmail.com</a>} taille='200px' classOf='contact-info' margin='10px' link={null} techno={null} visuel={Email} />
                 <CardFlip title='Localisation' description={<>Lille<br />Nantes</>} taille='200px' classOf='contact-info' margin='10px' link={null} techno={null} visuel={Localisation} />
             </div>
             <FormulaireContact />
